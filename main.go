03ead99dc7c441894ca9661a045ea7835c9829ce
GuiBondi <bondibiavatti@hotmail.com>
2024-09-15 21:33:26 -0300

    Book struct

--- main.go
+++ main.go
@@ -6,24 +6,31 @@ import (
     "net/http"
 )
 
-type Response struct {
-	Message string `json: "message"`
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   int    `json:"year"`
 }
 
-
-func  greetings(w http.ResponseWriter, r *http.Request) {
-	response := Response{Message: "Greetings!!"}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	json.NewEncoder(w).Encode(response)
-}
+// type Response struct {
+// 	Message string `json: "message"`
+// }
 
 
-func main(){
-	http.HandleFunc("/hello", greetings)
+// func  greetings(w http.ResponseWriter, r *http.Request) {
+// 	response := Response{Message: "Greetings!!"}
 
-	log.Println("Iniciando server na porta :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	json.NewEncoder(w).Encode(response)
+// }
+
+
+// func main(){
+// 	http.HandleFunc("/hello", greetings)
+
+// 	log.Println("Iniciando server na porta :8080")
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
 
